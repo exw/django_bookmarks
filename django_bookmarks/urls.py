5802b215a61c1ede4e374785018d2bac4ebb1960
exw <exw@openmailbox.org>
2015-05-27 22:56:02 -0500

    Initial view configuration

--- django_bookmarks/urls.py
+++ django_bookmarks/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from bookmarks import views as bookmarks 
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', bookmarks.main_page, name='main_page'),
 ]
